64b327eda277def6617d1776ba5dd98de1e42cf1
mywnajsldkf <mywnajsldkf@gmail.com>
2021-11-22 23:06:28 +0900

    algorithm

---
+++ baekjoon/1789.py
@@ -0,0 +1,15 @@
+# https://pacific-ocean.tistory.com/80
+
+
+s = int(input())
+
+count_n = 0
+
+cal_result = s
+i = 1
+
+while(cal_result - i >= 0):
+    cal_result = cal_result - i
+    i += 1
+
+print(i-1)
