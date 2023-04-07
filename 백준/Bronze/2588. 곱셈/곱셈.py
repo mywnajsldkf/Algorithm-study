e993bced6ad17f73e31eef39f957e8d574a1e423
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2023-04-07 09:49:02 +0900

    [Bronze III] Title: 곱셈, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -1,6 +1,16 @@
-a = int(input())
-b = str(input())
-print(a*int(b[2]))
-print(a*int(b[1]))
-print(a*int(b[0]))
-print(a*int(b))
+number = int(input())
+
+digit = list(map(int, input()))
+digit.reverse()
+
+result = 0
+
+ten = 1
+
+for i in digit:
+    result += number * i * ten
+    print(number * i)
+
+    ten *= 10
+
+print(result)
