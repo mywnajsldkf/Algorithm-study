a42566ea71666696def18efcfcc766af2adaf119
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2023-04-08 10:44:58 +0900

    [Bronze V] Title: 팩토리얼, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.py
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.py
@@ -1,9 +1,15 @@
-n = int(input())
+from sys import stdin as s
+
+N = int(s.readline())
 
 result = 1
 
-for i in range(n):
-    result *= n
-    n += -1
+if N == 0:
+    result = 1
+elif N == 1:
+    result = 1
+else:
+    for i in range(N, 1, -1):
+        result *= i
 
 print(result)
