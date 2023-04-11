0aef82a0e7e07dacb4c4121513c95e154b52a0d9
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2023-04-12 00:10:06 +0900

    [Gold IV] Title: N-Queen, Time: 3812 ms, Memory: 15340 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -2,50 +2,50 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
-    public static int N;
-    public static int[] arr;
-    public static int count = 0;
-
+    static int n;
+    static boolean[] visited;
+    static int[] arr;
+    static int count = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());   // 길이
-        arr = new int[N];
+        visited = new boolean[n];
+        arr = new int[n];
 
         nQueen(0);
-        System.out.println(count);
+        sb.append(count);
+        System.out.println(sb);
     }
 
     private static void nQueen(int depth) {
-        // 모든 원소를 다 채운 상태면 count를 증가하고 return한다.
-        if (depth == N) {
-            count++;
-            return;
+        if (depth == n){
+            count += 1;
         }
 
-        for (int i = 0; i < N; i++) {
+        for(int i = 0; i < n; i++){
+            if(visited[i]){
+                continue;
+            }
             arr[depth] = i;
-            if (togo(depth)) {
+            if(togo(depth)){
+                visited[i] = true;
                 nQueen(depth + 1);
+                visited[i] = false;
             }
         }
     }
 
-    // 놓을 위치가 다른 퀸으로부터 위협받는가
-    private static boolean togo(int col) {
-        for (int i = 0; i < col; i++) {
-            // 해당 열의 행과 i열의 행이 일치하는 경우(같은 행에 존재하는 경우)
-            if (arr[col] == arr[i]) {
+    private static boolean togo(int depth) {
+        for(int i = 0; i < depth; i++){
+            if(arr[i] == arr[depth]){
                 return false;
             }
-            // 대각선상에 놓인 경우
-            // 열의 차와 행의 차가 같으면 대각선에 놓인 경우이다.
-            else if (Math.abs(col - i) == Math.abs(arr[col] - arr[i])) {
+            else if (Math.abs(depth-i) == Math.abs(arr[depth]-arr[i])){
                 return false;
             }
         }
