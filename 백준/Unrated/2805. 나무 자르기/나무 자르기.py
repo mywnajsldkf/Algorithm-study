b1e5b66b7a31f8bbfe5a2f0e90d36a81b3a42220
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2023-04-14 18:06:42 +0900

    [Unrated] Title: 나무 자르기, Time: 512 ms, Memory: 259740 KB -BaekjoonHub

---
+++ 백준/Unrated/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,30 @@
+import sys
+from sys import stdin as s
+
+N, M = map(int, s.readline().split())
+
+tree = list(map(int, s.readline().split()))
+result = 0
+def buyTree(tree, target):
+    global result
+    left, right = 1, max(tree)
+
+    while(left <= right):
+        total = 0
+        middle = (left + right) // 2
+
+        for height in tree:
+            if (height - middle) >= 0:
+                total += (height - middle)
+
+            if total > target:
+                break
+
+        if total < target:
+            right = middle - 1
+        else:
+            left = middle + 1
+    result = right
+
+buyTree(tree, M)
+print(result)
