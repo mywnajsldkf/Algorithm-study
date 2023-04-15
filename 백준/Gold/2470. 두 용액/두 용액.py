a867066851896d83c223f783702802b2f226232b
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2023-04-15 21:02:17 +0900

    [Gold V] Title: 두 용액, Time: 120 ms, Memory: 42172 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -0,0 +1,28 @@
+from sys import stdin as s
+
+N = int(s.readline().rstrip())
+liquid = list(map(int, s.readline().rstrip().split(' ')))
+liquid.sort()
+
+left, right = 0, N-1
+
+answer = abs(liquid[right] + liquid[left])
+left_value, right_value = liquid[left], liquid[right]
+
+while left < right:
+    total = liquid[left] + liquid[right]
+
+    if abs(total) < answer:
+        answer = abs(total)
+        left_value = liquid[left]
+        right_value = liquid[right]
+        if answer == 0:
+            break
+    # 양수이면
+    if total > 0:
+        right -= 1
+    # 음수이면
+    else:
+        left += 1
+
+print(left_value, right_value)
