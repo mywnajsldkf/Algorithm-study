782adf8bfd0a7016d6a5083747b5fa5636395277
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2023-04-16 01:19:51 +0900

    [Silver I] Title: 곱셈, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -0,0 +1,14 @@
+from sys import stdin as s
+
+A, B, C = map(int, s.readline().split())
+def multiply(A, B, C):
+    if B == 1:
+        return A % C
+    # 홀수라면
+    if B % 2 == 1:
+        return (multiply(A, B // 2, C) ** 2) * A % C
+    # 짝수라면
+    else:
+        return (multiply(A, B // 2, C) ** 2) % C
+
+print(multiply(A, B, C))
