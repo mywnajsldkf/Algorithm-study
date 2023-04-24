46092dff76e8b4f8ae14c6d13766a9ece2bda31c
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2023-04-24 10:53:05 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 776 ms, Memory: 65100 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,42 +1,32 @@
 from sys import stdin as s
+from collections import deque
 
 N, M = map(int, s.readline().split())
 
-connection = [[0 for i in range(N+1)] for j in range(N+1)]
+connection = [[] for _ in range(N+1)]
 
 for i in range(M):
     u, v = map(int, s.readline().split())
-    connection[u][v] = 1
-    connection[v][u] = 1
+    connection[u].append(v)
+    connection[v].append(u)
 
 visited = [False for i in range(N+1)]
 
-queue = []
-count = 0
+def bfs(start):
+    queue = deque([start])
+    visited[start] = True
 
-
-def dfs(n):
-    queue.append(n)
     while len(queue) != 0:
-        # print(len(queue))
-        now = queue.pop()
-        # print(now)
-        visited[now] = True  # 방문 표시한다.
-        # print(visited)
-        for i in range(1, N+1):
-            # 방문했다면 -> continue
-            if visited[i]:
-                continue
-            #
-            if connection[now][i] == 1:
+        now = queue.popleft()
+        for i in connection[now]:
+            if not visited[i]:
                 queue.append(i)
+                visited[i] = True
 
-
+count = 0
 for i in range(1, N+1):
-    # 이전에 방문했다면, 탐색하지 않는다.
     if not visited[i]:
         count += 1
-        dfs(i)
+        bfs(i)
 
-# print(*connection, sep='\n')
 print(count)
