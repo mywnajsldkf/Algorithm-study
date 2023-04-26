efc3d4a4ea9c5dc5433672e575ebdfde73a6b7b2
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2023-04-26 13:14:08 +0900

    [Gold IV] Title: 미로만들기, Time: 48 ms, Memory: 33324 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.py
+++ 백준/Gold/2665. 미로만들기/미로만들기.py
@@ -1,6 +1,6 @@
 from sys import stdin as s
 import sys
-from collections import deque
+import heapq
 
 dy = [1, 0, -1, 0]
 dx = [0, 1, 0, -1]
@@ -11,35 +11,32 @@ miro = []
 for i in range(n):
     miro.append(list(map(int, s.readline().strip())))
 
-visited = [[-1 for _ in range(n)] for _ in range(n)]
+visited = [[0 for _ in range(n)] for _ in range(n)]
 
-def bfs():
-    queue = deque()
-    queue.append((0, 0))
-    visited[0][0] = 0
+def dijkstra(y, x):
+    hq = []
+    heapq.heappush(hq, (0, y, x))   # 비용, y좌표, x좌표
+    visited[y][x] = 1
 
-    while queue:
-        y, x = queue.popleft()
-        if y == n-1 and x == n-1:
-            return visited[y][x]
+    while hq:
+        cost, now_y, now_x = heapq.heappop(hq)
+        if now_y == n-1 and now_x == n-1:
+            return cost
 
-        for i in range(4):
-            n_y = y + dy[i]
-            n_x = x + dx[i]
-
-            # 범위 확인
-            if n_y < 0 or n_y >= n or n_x < 0 or n_x >= n:
+        for d in range(4):
+            new_y = now_y + dy[d]
+            new_x = now_x + dx[d]
+            if new_y < 0 or new_y >= n or new_x < 0 or new_x >= n:
                 continue
 
-            # 방문 확인
-            if visited[n_y][n_x] == -1:
-                if miro[n_y][n_x] == 1:
-                    # 가장 앞에 넣는다. -> 흰색 돌을 먼저 다보고
-                    queue.appendleft((n_y, n_x))
-                    # 흰색 돌은 방문 횟수를 변화시키지 않는다.
-                    visited[n_y][n_x] = visited[y][x]
+            if visited[new_y][new_x] == 0:
+                visited[new_y][new_x] = 1
+                # 흰방을 만나면
+                if miro[new_y][new_x] == 1:
+                    heapq.heappush(hq, (cost, new_y, new_x))
+                # 검은 방을 만나면, cost에 1 더한다.
                 else:
-                    queue.append((n_y, n_x))
-                    visited[n_y][n_x] = visited[y][x] + 1
+                    heapq.heappush(hq, (cost+1, new_y, new_x))
 
-print(bfs())
+
+print(dijkstra(0,0))
