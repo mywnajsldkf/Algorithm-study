2cb6a9254ca6a7c30f048c89fb34e02460935d6a
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2023-04-27 01:07:17 +0900

    [Gold V] Title: 토마토, Time: 1908 ms, Memory: 57516 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,60 +1,56 @@
+import sys
 from sys import stdin as s
 from collections import deque
 
-import sys
+M, N, H = list(map(int, s.readline().split()))
 
-M, N, H = map(int, s.readline().split())
+arr = [[list(map(int, s.readline().split())) for _ in range(N)] for _ in range(H)]
+visited = [[[False] * M for _ in range(N)] for _ in range(H)]
 
-dz = [0, -1, 0, 0, 1, 0]
-dy = [0, 0, 1, 0, 0, -1]
-dx = [1, 0, 0, -1, 0, 0]
+queue = deque()
 
-tomato = [[[] for _j in range(N)] for _k in range(H)]
-visited = [[[False for _i in range(M)] for _j in range(N)] for _k in range(H)]
+dz = [-1, 1, 0, 0, 0, 0]
+dy = [0, 0, -1, 1, 0, 0]
+dx = [0, 0, 0, 0, -1, 1]
 
-days = 0
-'''
-이렇게 보면 층같이 보인다.
-for i in tomato:
-    print(i)
-'''
+def bfs():
 
-for _i in range(H):
-    for _j in range(N):
-        tomato[_i][_j] = list(map(int, s.readline().split()))
-# print(tomato)
-queue = deque([])
+    while queue:
+        z, y, x = queue.popleft()
 
-for _i in range(H):
-    for _j in range(N):
-        for _k in range(M):
-            if tomato[_i][_j][_k] == 1:
-                queue.append([_i, _j, _k])
+        for i in range(6):
+            new_z = z + dz[i]
+            new_y = y + dy[i]
+            new_x = x + dx[i]
 
-while len(queue) != 0:
-    now = queue.popleft()
-    for i in range(6):
-        new_z = now[0] + dz[i]
-        new_y = now[1] + dy[i]
-        new_x = now[2] + dx[i]
+            if new_z < 0 or new_z >= H or new_y < 0 or new_y >= N or new_x < 0 or new_x >= M:
+                continue
 
-        # 범위 확인
-        if new_z < 0 or new_z >= H or new_y < 0 or new_y >= N or new_x < 0 or new_x >= M:
-            continue
+            # 값이 0이고 방문하지 않았다면 -> 이후에 방문한다.
+            if arr[new_z][new_y][new_x] == 0 and not visited[new_z][new_y][new_x]:
+                queue.append((new_z, new_y, new_x))
+                arr[new_z][new_y][new_x] = arr[z][y][x] + 1
+                visited[new_z][new_y][new_x] = True
 
-        # 방문 확인
-        if not visited[new_z][new_y][new_x] and tomato[new_z][new_y][new_x] == 0:
-            queue.append([new_z, new_y, new_x])
-            tomato[new_z][new_y][new_x] = tomato[now[0]][now[1]][now[2]] + 1
-            visited[new_z][new_y][new_x] = True
-            
+
 for _i in range(H):
     for _j in range(N):
         for _k in range(M):
-            if tomato[_i][_j][_k] == 0:
+            if arr[_i][_j][_k] == 1 and not visited[_i][_j][_k]:
+                queue.append((_i, _j, _k))
+                visited[_i][_j][_k] = True
+
+bfs()
+
+max_count = -sys.maxsize
+
+for i in arr:
+    for j in i:
+        max_count = max(max_count, max(j))
+        for k in j:
+            if k == 0:
                 print(-1)
-                exit(0)
-            else:
-                days = max(days, tomato[_i][_j][_k])
+                sys.exit()
 
-print(days-1)
+
+print(max_count - 1)
