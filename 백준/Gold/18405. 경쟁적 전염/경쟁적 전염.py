a59a79d91f5cb28a6cc49940a0b3cb16afbb5677
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2023-04-27 21:18:43 +0900

    [Gold V] Title: 경쟁적 전염, Time: 176 ms, Memory: 36288 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -14,38 +14,35 @@ for i in range(n):
 
 ss, y, x = map(int, s.readline().split())
 
-visited = [[False for _i in range(n)] for _j in range(n)]
+visited = [[0 for _ in range(n)] for _ in range(n)]
 
-def bfs():
-    while len(queue) > 0:
-        now_y, now_x = queue.popleft()
+tmp = []
 
-        for dir in range(4):
-            new_y = now_y + dy[dir]
-            new_x = now_x + dx[dir]
+for i in range(n):
+    for j in range(n):
+        # 바이러스가 채워져 있으면
+        if virus[i][j]:
+            tmp.append([virus[i][j], i, j])
+            visited[i][j] = virus[i][j]
 
-            # 범위 확인
-            if new_y < 0 or new_y >= n or new_x < 0 or new_x >= n:
+tmp.sort()
+q = deque()
+
+for num, now_y, now_x in tmp:
+    q.append([now_y, now_x, num])
+
+while len(q) != 0 and ss != 0:
+    size = len(q)
+    ss -= 1  # 바이러스 초를 줄인다.
+    for p in range(size):
+        now_y, now_x, num = q.popleft()
+        for i in range(4):
+            ny, nx = now_y + dy[i], now_x + dx[i]
+            if 0 > ny or ny >= n or 0 > nx or nx >= n:
                 continue
+            if visited[ny][nx]:
+                continue
+            visited[ny][nx] = num
+            q.append([ny, nx, num])
 
-            # 아직 채워지지 않았다면
-            if virus[new_y][new_x] == 0:
-                virus[new_y][new_x] = virus[now_y][now_x]
-
-            # 이미 채워진 지점이라면 이전 것과 비교하여 작은 것을 업데이트해준다.
-            if virus[new_y][new_x] != 0 and not visited[new_y][new_x]:
-                virus[new_y][new_x] = min(virus[new_y][new_x], virus[now_y][now_x])
-
-
-queue = deque()
-
-for t in range(ss):
-    for i in range(n):
-        for j in range(n):
-            # 방문하지 않았고,
-            if not visited[i][j] and virus[i][j] != 0:
-                visited[i][j] = True
-                queue.append((i, j))
-    bfs()
-
-print(virus[y - 1][x - 1])
+print(visited[y - 1][x - 1])
