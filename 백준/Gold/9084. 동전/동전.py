6ed70a6ee14e62f89f379d9e672bceae56737e90
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2023-04-29 19:41:35 +0900

    [Gold V] Title: 동전, Time: 84 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.py
+++ 백준/Gold/9084. 동전/동전.py
@@ -7,22 +7,15 @@ dp = [0] * 10001
 
 for _ in range(T):
     N = int(s.readline())
-    # coins = [0]
-    # coins.extend(list(map(int, s.readline().split())))
     coins = list(map(int, s.readline().split()))
-
     M = int(s.readline())  # 동전으로 만들어야하는 금액
 
-    dp = [[0] * (M + 1) for _ in range(N + 1)]
+    dp = [0] * (M + 1)
+    dp[0] = 1
 
-    for i in range(N + 1):
-        dp[i][0] = 1
+    for coin in coins:
+        for i in range(1, M+1):
+            if i >= coin:
+                dp[i] += dp[i-coin]
 
-    for j in range(1, N + 1):
-        for k in range(1, M + 1):
-            dp[j][k] = dp[j - 1][k]
-            # print(j)
-            if k - coins[j-1] >= 0:
-                dp[j][k] += dp[j][k - coins[j-1]]
-
-    print(dp[N][M])
+    print(dp[M])
