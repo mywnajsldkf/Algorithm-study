113ee5ea169798123994382b73c06bc03a5dbbe3
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2023-04-29 19:36:20 +0900

    [Gold V] Title: 동전, Time: 148 ms, Memory: 35420 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.py
+++ 백준/Gold/9084. 동전/동전.py
@@ -1,28 +1,28 @@
 from sys import stdin as s
 import sys
 
-
 T = int(s.readline())
 
 dp = [0] * 10001
 
-
 for _ in range(T):
     N = int(s.readline())
-    coins = [0]
-    coins.extend(list(map(int, s.readline().split())))
-    M = int(s.readline())   # 동전으로 만들어야하는 금액
+    # coins = [0]
+    # coins.extend(list(map(int, s.readline().split())))
+    coins = list(map(int, s.readline().split()))
 
-    dp = [[0] * (M+1) for _ in range(N+1)]
+    M = int(s.readline())  # 동전으로 만들어야하는 금액
 
-    for i in range(N+1):
+    dp = [[0] * (M + 1) for _ in range(N + 1)]
+
+    for i in range(N + 1):
         dp[i][0] = 1
 
-    for j in range(1, N+1):
-        for k in range(1, M+1):
-            dp[j][k] = dp[j-1][k]
+    for j in range(1, N + 1):
+        for k in range(1, M + 1):
+            dp[j][k] = dp[j - 1][k]
             # print(j)
-            if k-coins[j] >= 0:
-                dp[j][k] += dp[j][k-coins[j]]
+            if k - coins[j-1] >= 0:
+                dp[j][k] += dp[j][k - coins[j-1]]
 
     print(dp[N][M])
