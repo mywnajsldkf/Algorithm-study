ef897c40e5cb6bfee7abf49686753e71fe1dc109
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2023-05-01 10:29:16 +0900

    [Silver II] Title: 연속합, Time: 84 ms, Memory: 38964 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -0,0 +1,14 @@
+from sys import stdin as s
+import sys
+
+n = int(s.readline())
+
+arr = list(map(int, s.readline().split()))
+dp = arr
+
+def findMaxSum():
+    for i in range(1, len(dp)):
+        dp[i] = max(dp[i-1] + arr[i], dp[i])
+findMaxSum()
+
+print(max(dp))
