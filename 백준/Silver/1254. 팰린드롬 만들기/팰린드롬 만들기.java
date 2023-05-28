500c72a46f5f66e0600975bbdd9e0fe82137fe20
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2023-05-28 22:37:29 +0900

    [Silver II] Title: 팰린드롬 만들기, Time: 132 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Silver/1254. 팰린드롬 만들기/팰린드롬 만들기.java
+++ 백준/Silver/1254. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -1,38 +1,54 @@
+
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
-    static String s;
-    static int result;
+    static String[] s;
+    static int s_len;
+    static int left, right;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb_result = new StringBuilder();
-        s = br.readLine();
+        StringBuilder sb = new StringBuilder();
 
-        result = 0;
+        s = br.readLine().split("");
+        s_len = s.length;
+        int n =0;
+//        left = 0;
+//        right = s_len - 1;
 
-        if (s.equals(new StringBuilder(s).reverse().toString())) {
-            result = s.length();
-            sb_result.append(result);
-            System.out.println(sb_result);
-            System.exit(0);
-        }
+        for (int i = 0; i < s_len; i++) {
+            left = i;
+            right = s_len - 1;
 
-        for (int i = 1; i < s.length(); i++) {
-            StringBuilder sb = new StringBuilder(s);
-
-            sb.append(new StringBuilder(s.substring(0, i)).reverse());
-            if (sb.toString().equals(sb.reverse().toString())) {
-                result = sb.length();
+            if(!isPalindrome(left,right)){
+                n++;
+            }else{
                 break;
             }
+
+
         }
 
-        sb_result.append(result);
-        System.out.println(sb_result);
+        sb.append(s_len+n);
+        System.out.println(sb);
     }
+
+    private static boolean isPalindrome(int l, int r) {
+        while (l <= r) {
+            // 앞-뒤가 같다면
+            if (s[l].equals(s[r])) {
+                l++;
+                r--;
+            }
+            // 앞 뒤가 다르다면 -> 추가해줘야한다.
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
