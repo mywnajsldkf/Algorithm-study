ee4d907a940f8a1384781d12c4f8a5a54ba3bab7
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2023-08-23 18:27:58 +0900

    [Silver I] Title: 경로 찾기, Time: 192 ms, Memory: 16724 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int[][] G;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        G = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                G[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    // 갈 수 있는지 체크한다.
+                    if (G[i][k] == 1 && G[k][j] == 1) {
+                        G[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(G[i][j]);
+                sb.append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
