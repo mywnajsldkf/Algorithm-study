4f92c819da44d8269ecb10b2086a6893c252fdb7
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2023-10-11 14:28:18 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.09 ms, Memory: 69.8 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -3,41 +3,56 @@ import java.util.*;
 class Solution {
     public int solution(int n, int k) {
         int answer = 0;
-        StringBuffer number = new StringBuffer();
+        String[] temp = Integer.toString(n, k).split("0");
         
-        while(n > 0) {
-            number.append(String.valueOf(n % k));
-            n /= k;
-        }
-        
-        String translatedNumber = number.reverse().toString();
-        
-        List<Long> splited = new ArrayList<>();
-        
-        String num = "";
-        // boolean zero = false;
-        for(int i = 0; i < translatedNumber.length(); i++) {
-            if(translatedNumber.charAt(i) == '0' && num.length() != 0) {
-                splited.add(Long.parseLong(num));
-                num = "";
-                // zero = true;
+        for(String t : temp) {
+            if(t.length() == 0) continue;
+            long a = Long.parseLong(t);
+            if(a == 1) continue;
+            boolean isPrime = true;
+            for(int i = 2; i <= Math.sqrt(a); i++) {
+                if(a % i == 0) {
+                    isPrime = false;
+                    break;
+                }
             }
-            else if(translatedNumber.charAt(i) != '0') {
-                num += String.valueOf(translatedNumber.charAt(i));
-            }
-        }
-        
-        if(num.length() != 0) {
-            splited.add(Long.parseLong(num));
-        }
-                
-        for(int i = 0; i < splited.size(); i++) {
-            Long intNum = splited.get(i);
             
-            if(isPrime(intNum)) {
-                answer += 1;
-            }
+            if(isPrime) answer++;
         }
+        
+//         while(n > 0) {
+//             number.append(String.valueOf(n % k));
+//             n /= k;
+//         }
+        
+//         String translatedNumber = number.reverse().toString();
+        
+//         List<Long> splited = new ArrayList<>();
+        
+//         String num = "";
+//         // boolean zero = false;
+//         for(int i = 0; i < translatedNumber.length(); i++) {
+//             if(translatedNumber.charAt(i) == '0' && num.length() != 0) {
+//                 splited.add(Long.parseLong(num));
+//                 num = "";
+//                 // zero = true;
+//             }
+//             else if(translatedNumber.charAt(i) != '0') {
+//                 num += String.valueOf(translatedNumber.charAt(i));
+//             }
+//         }
+        
+//         if(num.length() != 0) {
+//             splited.add(Long.parseLong(num));
+//         }
+                
+//         for(int i = 0; i < splited.size(); i++) {
+//             Long intNum = splited.get(i);
+            
+//             if(isPrime(intNum)) {
+//                 answer += 1;
+//             }
+//         }
 
         return answer;
     }
